961755f077d93dc30b3e27e2e9734ae87c0166b9
joaovaladao <joaovaladao161274@gmail.com>
2023-02-08 11:57:18 -0300

    Código para aferição de temperatura

---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+
+#include "dht11.h"
+#include "freertos/task.h"
+
+void app_main()
+{
+    DHT11_init(GPIO_NUM_4);
+
+    while(1) {
+        printf("Temperature is %d \n", DHT11_read().temperature);
+        printf("Humidity is %d\n", DHT11_read().humidity);
+        printf("Status code is %d\n", DHT11_read().status);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
